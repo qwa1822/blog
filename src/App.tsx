076c176dfc7057b3bc7fe85a45570536549d99f4
qwa1822 <asdzxc1822@naver.com>
2023-12-20 17:20:58 +0900

    'update'

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+
+function App() {
+  return <h2>Hello</h2>;
+}
+
+export default App;
